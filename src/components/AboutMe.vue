b575e6ef80c1cda5aab3b3db85ed38ef33da00d0
Jose Renteria <JoseRenteria.sf@outlook.com>
2018-11-25 17:55:40 -0800

    testing iphone x issue with p text

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -6,9 +6,12 @@
                 <h2 class="title is-spaced is-size-1-desktop is-size-2-tablet is-size-3-mobile">About Me</h2>
                 <!-- <h2 class="subtitle">Front-end Developer</h2> -->
                 <p class="content">I'm a late bloomer when it comes to tech. Decided to go back to school at 32 to get my Associates in Computer Science from Kingsborough Community College in New York City. After that I completed an intensive web development bootcamp program at Fullstack Academy. I then completed an unpaid intership at ForwardLane. Build a small web application for Estee Lauder and now I am working as a QA Analyst for Switchfly in San Francisco.  
+                <br/>
+                <br/>
+                <strong class="is-size-5">Seeking a junior Front-End developer or QA Automation engineering position with a company with great mentorship and support. </strong>
                 </p>
                 <!-- <br/>  -->
-                 <strong class="is-size-5">Seeking a junior Front-End developer or QA Automation engineering position with a company with great mentorship and support. </strong>
+                 
             </div>
         </div>
     </section>
