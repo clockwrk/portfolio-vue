7ecbb7a8b791fe844b45103d41e53f2a9d77bef8
Jose Renteria <JoseRenteria.sf@outlook.com>
2019-05-05 11:27:36 -0700

    update text

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -5,9 +5,9 @@
                 <h2 class="title is-spaced is-size-1-desktop is-size-2-tablet is-size-3-mobile">About Me</h2>
                 <p class="content">Went back to school to get my Associates in Computer Science from Kingsborough Community College in New York City. Completed an intensive web development bootcamp program at Fullstack Academy and a unpaid web developer internship at ForwardLane. Built a small web application for Estee Lauder. Began working in Quality Assurance at Switchfly in San Francisco. Now working for CBS Interactive as Video Quality Assurance Analyst. Going back to parttime school in the Fall of 2019 to San Francisco State University to complete my Bachelors of Science in Computer Science.
                 </p>
+                <!-- <br/> -->
                 <br/>
-                <br/>
-                <strong class="is-size-5">Looking for a intern or junior position for a front-end developer or QA automation engineering position with a company with great mentorship and support. </strong>
+                <strong class="is-size-5">Looking for a internship or junior level position for a front-end developer or QA automation engineering with a company with great mentorship and support. </strong>
             </div>
         </div>
     </section>
